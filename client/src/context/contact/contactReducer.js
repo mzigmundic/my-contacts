6cc9ad4d6918ae59486e41e139179721ddf587eb
mzxrandom <mzx.random@gmail.com>
2020-09-06 16:46:38 +0200

    Update Contact

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -29,9 +29,12 @@ export default (state, action) => {
             return {
                 ...state,
                 contacts: state.contacts.map((contact) =>
-                    contact.id === action.payload.id ? action.payload : contact
+                    contact._id === action.payload._id
+                        ? action.payload
+                        : contact
                 ),
                 loading: false,
+                current: null,
             };
         case DELETE_CONTACT:
             return {
