20227b6fbb01fadcdec0621565cf992dff16fa7a
mzxrandom <mzx.random@gmail.com>
2020-09-06 15:55:39 +0200

    Add and Get Contacts

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,23 +1,28 @@
 import React, { Fragment, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import AuthContext from '../../context/auth/authContext';
+import AuthContext from "../../context/auth/authContext";
+import ContactContext from "../../context/contact/contactContext";
 
 const Navbar = ({ title, icon }) => {
-    const authContext = useContext(AuthContext); 
+    const authContext = useContext(AuthContext);
+    const contactContext = useContext(ContactContext);
 
     const { isAuthenticated, logout, user } = authContext;
+    const { clearContacts } = contactContext;
 
     const onLogout = () => {
         logout();
-    }
+        clearContacts();
+    };
 
     const authLinks = (
         <Fragment>
-            <li>Hello { user && user.name }</li>
+            <li>Hello {user && user.name}</li>
             <li>
                 <a onClick={onLogout} href="#!">
-                    <i className="fas fa-sign-out-alt"></i> <span className="hide-sm">Logout</span>
+                    <i className="fas fa-sign-out-alt"></i>{" "}
+                    <span className="hide-sm">Logout</span>
                 </a>
             </li>
         </Fragment>
@@ -39,9 +44,7 @@ const Navbar = ({ title, icon }) => {
             <h1>
                 <i className={icon} /> {title}
             </h1>
-            <ul>
-                {isAuthenticated ? authLinks : guestLinks}
-            </ul>
+            <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
         </div>
     );
 };
